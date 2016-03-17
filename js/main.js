2dbae6f54fe6f3d0ded9cbf54e079ae2bd82f1a0
Shijir Tsogoo <shijir99@gmail.com>
2016-03-16 23:48:54 -0700

    everything added in

--- js/main.js
+++ js/main.js
@@ -402,13 +402,21 @@ var gravitySimulation = (function() {
   gravity = new Gravity(300);
 
 });
-var timeDilationSimulations = (function () {
+var timeDilationSimulations = (function() {
+  
+
+  // setup constants
   var canvasX = 250;
-  var canvasY = 50;
-  var canvasDim = 500;
-  var centerOffset = canvasDim / 2;
-  var interval = 10;
+  var canvasY = 0;
+  var canvasWidth = 600;
+  var canvasHeight = 500;
+  var centerYOffset = canvasHeight / 2;
+  var centerXOffset = canvasWidth / 2;
+  const interval = 3; // DO NOT MODIFY THIS ONE
+  var animationState = 1; // 1 for play
   var referenceTime = 0;
+  
+  var paper = Raphael(document.getElementById('timeDilationSimulationContainer'), canvasWidth, canvasHeight);
 
   // center object mass settings
   var centerObjectMass = 5000000000;
@@ -421,38 +429,44 @@ var timeDilationSimulations = (function () {
 
   // orbiting objects
   var orbitingObjects = [];
-
-  var paper = Raphael(document.getElementById('timeDilationSimulationContainer'), canvasDim, canvasDim);
-  var centerObject = paper.circle(centerOffset, centerOffset, centerObjectSize);
-  centerObject.attr("fill", "#000").attr("stroke", "#fff");
-
   addObject('earth', 150, "blue"); //debug
   addObject('mars', 200, "red"); //debug
 
-  setInterval(function(){
+  // center mass/black hole
+  var centerObject = paper.circle(centerXOffset, centerYOffset, centerObjectSize);
+  centerObject.attr("fill", "#000").attr("stroke", "#000");
 
-    referenceTime += interval;
+  // initialize movement
+  movePlanets();
+
+  function movePlanets(){
+    var timePassed = interval * animationState;
+    referenceTime += timePassed;
+    $('#center_mass').text(centerObjectMass);
     $('#reference').text(referenceTime);
 
     orbitingObjects.forEach(function(object) {
       var velocity = Math.sqrt(gravitationalConstant * centerObjectMass / object.radius);
-      object.theta = object.theta + (velocity / object.radius * interval);
-      var dx = object.radius * Math.cos(object.theta) + centerOffset - object.x;
-      object.x = object.x + dx;
-      var dy = object.radius * Math.sin(object.theta) + centerOffset - object.y;
-      object.y = object.y + dy;
-      object.raphaelObj.translate(dx, dy);
+      object.theta = object.theta + (velocity / object.radius * timePassed);
+      object.x = object.radius * Math.cos(object.theta);
+      object.y = object.radius * Math.sin(object.theta);
+      object.raphaelObj.transform('t' + object.x + ',' + object.y);
 
       var amountAhead = referenceTime - calculateAge(centerObjectMass, object.radius);
 
       var timeSelector = $("#" + object.name);
       if (!timeSelector.length) {
-        $("#sidebar").append("<p>" + object.name + " is  ahead by: <span id=" + object.name + "></span></p>")
+        $("#relativeTimes").append("<p>" + object.name + " is  ahead by: <span id=" + object.name + "></span></p>")
       }
       timeSelector.text(amountAhead)
     });
 
-  }, interval);
+    if (animationState) { // if animationState is 0, break to save memory
+      setTimeout(function () {
+        movePlanets();
+      }, interval);
+    } //else {console.log('failings')} // debug code
+  }
 
   function calculateAge(mass, radius) {
     // relative time
@@ -460,22 +474,13 @@ var timeDilationSimulations = (function () {
         (1/radius) * (3 * gravitationalConstant * mass)/speedOfLight);
   }
 
-  function getCircleToPath(x, y, r){ //x and y are center, r is radius
-    return 'M ' +
-      x + ',' + (y-r)+
-      ' A ' + r + ',' + r +
-      ' 45 1,1 ' +
-      (x-0.1) + ',' + (y-r) +
-      ' z';
-  }
-
   // add object to system
   function addObject(name, radius, color) {
     if (radius < lowerBound){
       return false;
     }
-    var x = radius * Math.cos(0) + centerOffset;
-    var y = radius * Math.sin(0) + centerOffset;
+    var x = centerXOffset;
+    var y = centerYOffset;
     orbitingObjects.push({
       name: name,
       radius: radius,
@@ -484,11 +489,73 @@ var timeDilationSimulations = (function () {
       theta: 0,
       raphaelObj: paper.circle(x, y, 10).attr("fill", color).attr("stroke", color) //arbitrary size 10
     });
-
-    //paper.circle(x, y, 10).attr("fill", color).attr("stroke", color).animate({
-    //  path: getCircleToPath(centerOffset, centerOffset, radius)
-    //}, 500, 'bounce')
   }
+
+  $('#button_stop').click(function(){
+    console.log("restart invoked.");
+    orbitingObjects.forEach(function(d) {
+      d.theta = 0;
+    });
+    animationState = 0;
+    referenceTime = 0;
+    // code to reset
+  });
+
+  $('#button_pause').click(function() {
+    console.log("pause invoked.");
+    animationState = 0;
+  });
+
+  $('#button_play').click(function() {
+    console.log("play invoked. " + animationState);
+    var lastState = animationState;
+    animationState = 1;
+    if (lastState === 0) {
+      movePlanets();
+    }
+  });
+
+  $('#button_fw').click(function(){
+    console.log("button forward invoked.");
+    var lastState = animationState;
+    animationState = 2;
+    if (lastState === 0) {
+      movePlanets();
+    }
+  });
+
+  $('#button_ffw').click(function(){
+    console.log("button fast forward invoked.");
+    var lastState = animationState;
+    animationState = 4;
+    if (lastState === 0) {
+      movePlanets();
+    }
+  });
+
+  $('#increase_mass').click(function() {
+    console.log("increasing mass");
+    centerObjectMass = centerObjectMass * 2
+  });
+
+  $('#decrease_mass').click(function() {
+    console.log("increasing mass");
+    if (centerObjectMass > 250000000) {
+      centerObjectMass = centerObjectMass / 2
+    }
+  });
+
+    $( "#addParticleButton" ).click(function() {
+    var particleName = $('#particleName').val();
+    var particleRadius = $('#particleRadius').val();
+    var particleColor = $('#particleColor').val();
+        console.log("Adding Particle of Name: " + particleName + " Radius: " + particleRadius
+      + " Color: " + particleColor);
+    if (particleName && particleColor && particleRadius) {
+      addObject(particleName, particleRadius, particleColor)
+    }
+    });
+
 });
 actionsInViewports = (function() {
 
